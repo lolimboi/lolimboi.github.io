46aa84bb5925210996dc5cbe4f88897d458a9ce6
lolimboi <weckertlogan@gmail.com>
2021-09-24 12:09:56 -0600

    added traffic light
    beep beep

---
+++ traffic light/sketch.js
@@ -0,0 +1,18 @@
+// AAAAAA GOD WHY MUST I SUFFER
+// Logan Weckert
+// 9/24/2021
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  fill(random(255));
+  ellipse(mouseX, mouseY, 100, 100);
+}
