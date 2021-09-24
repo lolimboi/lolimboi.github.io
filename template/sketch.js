39130b7d464f264a8b5fcb0cf5c44716b176af82
lolimboi <weckertlogan@gmail.com>
2021-09-24 12:01:33 -0600

    Added template
    I added a template baybee

---
+++ template/sketch.js
@@ -0,0 +1,17 @@
+// AAAAAA GOD WHY MUST I SUFFER
+// Logan Weckert
+// 9/24/2021
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  rect(mouseX, mouseY, 100, 100);
+}
