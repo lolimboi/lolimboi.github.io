5fd29d685608c4898506229baebc69bbd51c7029
lolimboi <weckertlogan@gmail.com>
2021-11-09 00:13:26 -0600

    "finished" the game

--- Battle maze/sketch.js
+++ Battle maze/sketch.js
@@ -1,6 +1,6 @@
-// Title
+// Puzzle Maze (grid based game)
 // Logan Weckert
-// x/xx/2021
+// 8/11/2021
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -9,201 +9,205 @@ let gridSize = 20;
 let grid;
 let cellWidth, cellHeight;
 let level;
-let hurt, lastTimeSwitched;
+let lastTimeSwitched;
 let player1; 
+let leftkey, rightkey, upkey, downkey;
+let win = false;
+let begin = true;
 
-function preload(){
-  level = loadJSON("assets/level1.json");
-}
-
-function setup() {
-  createCanvas(windowHeight, windowHeight);
-  grid = level;
-  cellWidth = width/gridSize;
-  cellHeight = height/gridSize;
-  hurt = false;
-  lastTimeSwitched = 0;
-  player1 = new Player(0, 0, 100);
-}
-
-function draw() {
-  background(220);
-  frameRate(20);
- 
-
-  displayGrid();
-
-  player1.playerMove();
-  player1.movement();
-  player1.death();
-  player1.keyPressed();
-  harm();
-}
-
-function createEmpty2DArray(rows, cols){
-  let board =[];
-  for (let y=0; y<rows; y++){
-    board.push([]);
-    for(let x = 0; x<cols; x++){
-      board[y].push(0);
-    }
-  }
-  return board;
-}
-
-
-function displayGrid(){
-  for(let y=0; y<gridSize; y++){
-    for(let x=0; x<gridSize; x++){
-      if(grid[y][x] === 0){
-        fill("white");
-      }
-      if (grid[y][x] === 1){
-        fill("black");
-      }
-      if(grid[y][x] === 9){
-        if (hurt){
-          fill("purple");
-        }
-        else{
-          fill("red");
-        }  
-      }
-      if(grid[y][x] === 2){
-        fill("blue");
-      }
-      noStroke();
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
-    }
-  }
-}
-
-function mousePressed(){
-  let cellX = Math.floor(mouseX/cellWidth);
-  let cellY = Math.floor(mouseY/cellHeight);
-
-  if (grid[cellY][cellX] === 0){
-    grid[cellY][cellX] = 1;
-  }
-  else if (grid[cellY][cellX] === 1){
-    grid[cellY][cellX] = 0;
-  }
-}
-
-function keyPressed(){
-  if (key === "o"){
-    grid = createEmpty2DArray(gridSize, gridSize);
-  }
-  if (key === "r"){
-    grid = createRandom2DArray(gridSize, gridSize);
-  }
-  if(key === "n"){
-    let cellX = Math.floor(mouseX/cellWidth);
-    let cellY = Math.floor(mouseY/cellHeight);
-
-    if (grid[cellY][cellX] === 0 || grid[cellY][cellX] === 1){
-      grid[cellY][cellX] = 2;
-    }
-    else if (grid[cellY][cellX] === 2){
-      grid[cellY][cellX] = 0;
-    }
+  function preload(){
+    level = loadJSON("assets/level1.json");
   }
 
-  function createRandom2DArray(rows, cols){
-    let board =[];
-    for (let y=0; y<rows; y++){
-      board.push([]);
-      for(let x = 0; x<cols; x++){
-        if (random(100) < 50){
-          board[y].push(0);
-        }
-        else {
-          board[y].push(1);
-        }
-      }
-    }
-    return board;
+  function setup() {
+    createCanvas(windowHeight, windowHeight);
+    grid = level;
+    cellWidth = width/gridSize;
+    cellHeight = height/gridSize;
+    hurt = false;
+    lastTimeSwitched = 0;
+    player1 = new Player(0, 0);
+    leftkey = false;
+    rightkey = false;
+    upkey = false;
+    downkey = false;
   }
-}
-function harm(){
 
-  if(hurt){
-    if (millis() > lastTimeSwitched + 2000) {
-      lastTimeSwitched = millis();
-      hurt = !hurt;
-    }
-    console.log(millis());
-  }
-}
 
-class Player{
-  constructor(playerX, playerY, playerHealth){
-    this.playerX = playerX;
-    this.playerY = playerY;
-    this.playerHealth = playerHealth;
-  }
-  playerMove(newX, newY){
-    if (newX >= 0 && newY >= 0 && newX < gridSize && newY < gridSize){
+
+  function draw() {
+    background(220);
+    frameRate(20);
   
-      if (grid[newY][newX] === 0){
-  
-        grid[this.playerY][this.playerX] = 0;
-        this.playerX = newX;
-        this.playerY = newY;
-        grid[this.playerY][this.playerX] = 9;
-      }
-    }
-  }
-  movement(){
-    if (keyIsDown(68)){
-      this.playerMove(this.playerX+1, this.playerY);
-    }
-    if (keyIsDown(87)){
-      this.playerMove(this.playerX, this.playerY-1);
-    }
-    if (keyIsDown(83)){
-      this.playerMove(this.playerX, this.playerY+1);
-    }
-    if (keyIsDown(65)){
-      this.playerMove(this.playerX-1, this.playerY);
-    }
-    if (keyIsDown(72)){
-      if(hurt === false){
-        this.playerHealth -= 10;
-        hurt = true;
-      }
-      console.log(this.playerHealth);
-      
-    }
-  }
-  death(){
-    if (this.playerHealth <= 0){
-      grid[this.playerY][this.playerX] = 0;
-    }
-  } 
-  keyPressed(){
-    if (this.playerX-1 >= 0 && this.playerY-1 >= 0 && this.playerX+1 < gridSize && this.playerY+1 < gridSize) {
-      if(grid[this.playerY][this.playerX-1] === 2){
-        if (keyCode === 37){
-          grid[this.playerY][this.playerX-1] = 0;
-        }
-      }
 
-      if (grid[this.playerY][this.playerX+1] === 2){
-        if(keyCode === 39){
-          grid[this.playerY][this.playerX+1] = 0;
+    displayGrid();
+
+    player1.playerMove();
+    player1.movement();
+    player1.keyPressed();
+  }
+
+//displays each tile
+  function displayGrid(){
+    for(let y=0; y<gridSize; y++){
+      for(let x=0; x<gridSize; x++){
+        if(grid[y][x] === 0){
+          fill("white");
+        }
+        if (grid[y][x] === 1){
+          fill("black");
+        }
+        if(grid[y][x] === 9){
+          if (hurt){
+            fill("purple");
+          }
+          else{
+            fill("red");
+          }  
+        }
+        if(grid[y][x] === 2){
+          fill("blue");
+        }
+        if(grid[y][x] === 3){
+          fill("green");
+        }
+        if(grid[y][x] === 4){
+          fill("grey");
+        }
+        if(grid[y][x] === 5){
+          fill("green");
+        }
+        if(grid[y][x] === 6){
+          fill("green");
+        }
+        if(grid[y][x] === 7){
+          fill("green");
+        }
+        if(grid[y][x] === 8){
+          fill("yellow");
+        }
+        noStroke();
+        rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      }
+    }
+  }
+
+  //creates a player object, carrying all the needed bits
+ class Player{
+    constructor(playerX, playerY){
+      this.playerX = playerX;
+      this.playerY = playerY;
+    }
+    //establishes the players posotion
+    playerMove(newX, newY){
+      if (newX >= 0 && newY >= 0 && newX < gridSize && newY < gridSize){
+    
+        if (grid[newY][newX] === 0){
+    
+          grid[this.playerY][this.playerX] = 0;
+          this.playerX = newX;
+          this.playerY = newY;
+          grid[this.playerY][this.playerX] = 9;
         }
       }
-      if (grid[this.playerY-1][this.playerX] === 2){
-        if(keyCode === 38){
-          grid[this.playerY-1][this.playerX] = 0;
+    }
+    //actually moves the player mathematically
+    movement(){
+      if (keyIsDown(68)){
+        this.playerMove(this.playerX+1, this.playerY);
+      }
+      if (keyIsDown(87)){
+        this.playerMove(this.playerX, this.playerY-1);
+      }
+      if (keyIsDown(83)){
+        this.playerMove(this.playerX, this.playerY+1);
+      }
+      if (keyIsDown(65)){
+        this.playerMove(this.playerX-1, this.playerY);
+      }
+    }
+    keyPressed(){
+      //makes the player able to break blue blocks from certain sides
+      if (this.playerX-1 >= 0 && this.playerY-1 >= 0 && this.playerX+1 < gridSize && this.playerY+1 < gridSize) {
+        if(leftkey){
+          if(grid[this.playerY][this.playerX-1] === 2){
+            if (keyCode === 37){
+              grid[this.playerY][this.playerX-1] = 0;
+          }
         }
       }
-      if (grid[this.playerY+1][this.playerX] === 2){
-        if(keyCode === 40){
-          grid[this.playerY+1][this.playerX] = 0;
+        if(rightkey){
+          if (grid[this.playerY][this.playerX+1] === 2){
+            if(keyCode === 39){
+              grid[this.playerY][this.playerX+1] = 0;
+          }
+        }
+      }
+        if(upkey){
+          if (grid[this.playerY-1][this.playerX] === 2){
+            if(keyCode === 38){
+              grid[this.playerY-1][this.playerX] = 0;
+          }
+        }
+      }
+        if(downkey){
+          if (grid[this.playerY+1][this.playerX] === 2){
+            if(keyCode === 40){
+              grid[this.playerY+1][this.playerX] = 0;
+          }
+        }
+      }  
+      //makes the player able to gain items
+      if (grid[this.playerY][this.playerX-1] === 3 ){
+        if(keyCode === 69){
+          leftkey = true;
+          grid[this.playerY][this.playerX-1] = 4;
+        }
+      }
+      if(grid[this.playerY-1][this.playerX] === 3){
+        if(keyCode === 69){
+          leftkey = true;
+          grid[this.playerY-1][this.playerX] = 4;
+        }
+      }
+      if (grid[this.playerY-1][this.playerX] === 5 ){
+        if(keyCode === 69){
+          rightkey = true;
+          grid[this.playerY-1][this.playerX] = 4;
+        }
+      }
+      if (grid[this.playerY][this.playerX-1] === 6 ){
+        if(keyCode === 69){
+          upkey = true;
+          grid[this.playerY][this.playerX-1] = 4;
+        }
+      }
+      if(grid[this.playerY+1][this.playerX] === 6){
+        if(keyCode === 69){
+          upkey = true;
+          grid[this.playerY+1][this.playerX] = 4;
+        }
+      }
+        if (grid[this.playerY][this.playerX+1] === 7){
+          if(keyCode === 69){
+            downkey = true;
+            grid[this.playerY][this.playerX+1] = 4;
+          }
+        }
+        if( grid[this.playerY+1][this.playerX] === 7){
+          if(keyCode === 69){
+            downkey = true;
+            grid[this.playerY+1][this.playerX] = 4;
+          }
+        }
+        //makes the player win when interacting with the door
+        if (grid[this.playerY-1][this.playerX] === 8 ){
+          if(keyCode === 69){
+            background(0);
+            textAlign(RIGHT, BASELINE);
+            text('Congrats!', 0, 0);
+          }
         }
       }
     }
   }
-}
